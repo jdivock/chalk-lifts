df2fd1fd0149373176b66d3f90c8f4d46f506bc1
Jason Divock <jdivock@box.com>
2015-10-02 18:24:32 -0700

    alright, weird identifier stuff hooked up, whatever that's doing

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
 import Schema from './data/Schema';
+import DebugOut from 'debug';
+import { printSchema } from 'graphql/utilities';
+
+let debug = DebugOut('server.js');
 
 const GRAPHQL_PORT = 8080;
 
@@ -13,3 +17,6 @@ graphQLServer.use('/graphql', graphQLHTTP({schema: Schema, pretty: true}));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
+
+// Pretty print schema
+debug(printSchema(Schema));
