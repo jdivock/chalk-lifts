f04771caaf46633fae085d71571f5b69be03a1d0
Jason Divock <jdivock@gmail.com>
2016-02-15 17:29:00 -0800

    Spent 2 hours playing with webpack and babel. No pages build, 2016 WEB DEVELOPMENT EVERYBODY

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
+/* eslint no-console: 0 */
+import Schema from './data/Schema';
+import WebpackDevServer from 'webpack-dev-server';
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
-import Schema from './data/Schema';
-import debugOut from 'debug';
-import { printSchema } from 'graphql/utilities';
-
-const debug = debugOut('server.js');
+import path from 'path';
+import webpack from 'webpack';
 
 const GRAPHQL_PORT = 8080;
+const APP_PORT = 3000;
 
 // Expose a GraphQL endpoint
 const graphQLServer = express();
 
-graphQLServer.use('/', express.static('public'));
-
 graphQLServer.use('/graphql', graphQLHTTP({ schema: Schema, pretty: true }));
-graphQLServer.listen(GRAPHQL_PORT, () => debug(
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
 
-// Pretty print schema
-debug(printSchema(Schema));
+// Serve the Relay app
+const compiler = webpack({
+  entry: path.resolve(__dirname, 'js', 'app.js'),
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.js$/,
+        cacheDirectory: true,
+        presets: ['es2015', 'react', 'stage-0'],
+      },
+    ],
+  },
+  output: { filename: 'app.js', path: '/' },
+});
+
+const app = new WebpackDevServer(compiler, {
+  contentBase: '/public/',
+  proxy: { '/graphql': `http://localhost:${GRAPHQL_PORT}` },
+  publicPath: '/js/',
+  stats: { colors: true },
+});
+
+app.use('/', express.static(path.resolve(__dirname, 'public')));
+app.listen(APP_PORT, () => {
+  console.log(`App is now running on http://localhost:${APP_PORT}`);
+});
