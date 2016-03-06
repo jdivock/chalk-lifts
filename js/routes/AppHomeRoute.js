130d87fd657c3f959f2bcb4e563a7c0b15753c48
Jason Divock <jdivock@gmail.com>
2016-03-05 18:09:01 -0800

    added dev target for hotloading

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -4,7 +4,7 @@ export default class extends Relay.Route {
   static queries = {
     account: () => Relay.QL`
       query {
-        account(id:1)
+        account(email:'jdivock@jdivock.com')
       }
     `,
   };
