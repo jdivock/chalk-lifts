96dcc61f9d73559046cb7124e4150d02f8b24ee8
Jason Divock <jdivock@gmail.com>
2016-03-19 15:46:03 -0700

    Updated relay componentsk

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -4,7 +4,7 @@ export default class extends Relay.Route {
   static queries = {
     account: () => Relay.QL`
       query {
-        account(email:"jay@jay.com")
+        user(email:"jay@jay.com")
       }
     `,
   };
